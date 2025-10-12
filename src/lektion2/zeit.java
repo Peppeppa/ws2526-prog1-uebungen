ae42570036f4668e455cb26b7ac51b3bf3ecc9b9
Stephen Eyring <accounts@eyring-s.de>
2025-10-12 21:54:41 +0200

    Lektion2 Übung zeit

--- src/lektion2/zeit.java
+++ src/lektion2/zeit.java
@@ -13,19 +13,19 @@ public class zeit {
         System.out.println("Die Uhrzeit ist " + stunden + ":" + minuten + ":" + sekunden);
 
         // Sekunden seit Mitternacht
-        int sekSeitMitternacht;
+        double sekSeitMitternacht;
         sekSeitMitternacht = ((stunden * 60) + minuten) * 60 + sekunden;
         System.out.println("Seit Mitternacht sind " + sekSeitMitternacht + " Sekunden vergangen.");
 
         // sekunden bis Mitternacht
-        int sek24H = 24 * 60 * 60;
-        int secBisMitternacht;
+        double sek24H = 24 * 60 * 60;
+        double secBisMitternacht;
         secBisMitternacht = sek24H - sekSeitMitternacht;
         System.out.println("Bis Mitternacht sind es " + secBisMitternacht + " Sekunden.");
 
         // Prozentsatz des vergangen Tages
         double prozentDesTages;
-        prozentDesTages = ((double) sekSeitMitternacht / sek24H) * 100;
+        prozentDesTages = ( sekSeitMitternacht / sek24H) * 100;
         System.out.println("Es sind " + prozentDesTages + " % des Tages vergangen");
 
 
