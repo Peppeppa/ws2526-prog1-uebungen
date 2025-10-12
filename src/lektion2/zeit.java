16aa0494f9f716f69a34fdf7ee185e5a2e3c4a6e
Stephen Eyring <accounts@eyring-s.de>
2025-10-12 21:36:25 +0200

    Lektion2 Übung zeit

--- src/lektion2/zeit.java
+++ src/lektion2/zeit.java
@@ -1,15 +1,34 @@
 package lektion2;
 
+import java.time.LocalTime;
+
 public class zeit {
     //12.10.2025
     public static void main(String[] args) {
 
-        int stunden;
-        int minuten;
-        int sekunden;
+        int stunden = LocalTime.now().getHour();
+        int minuten = LocalTime.now().getMinute();
+        int sekunden = LocalTime.now().getSecond();
 
-        java.time.LocalTime time = java.time.LocalTime.now();
-        System.out.printf("%02d:%02d%n", time.getHour(), time.getMinute());
+        System.out.println("Die Uhrzeit ist " + stunden + ":" + minuten + ":" + sekunden);
+
+        // Sekunden seit Mitternacht
+        int sekSeitMitternacht;
+        sekSeitMitternacht = ((stunden * 60) + minuten) * 60 + sekunden;
+        System.out.println("Seit Mitternacht sind " + sekSeitMitternacht + " Sekunden vergangen.");
+
+        // sekunden bis Mitternacht
+        int sek24H = 24 * 60 * 60;
+        int secBisMitternacht;
+        secBisMitternacht = sek24H - sekSeitMitternacht;
+        System.out.println("Bis Mitternacht sind es " + secBisMitternacht + " Sekunden.");
+
+        // Prozentsatz des vergangen Tages
+        double prozentDesTages;
+        prozentDesTages = ((double) sekSeitMitternacht / sek24H) * 100;
+        System.out.println("Es sind " + prozentDesTages + " % des Tages vergangen");
+
+
     }
 
 }
