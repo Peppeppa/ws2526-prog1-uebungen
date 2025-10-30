a327fcc37397f4d3f1888899f84e8f1c6e67bb0c
Stephen Eyring <accounts@eyring-s.de>
2025-10-30 17:31:42 +0100

    Temperatur

--- src/lektion4/Temperaturtabelle.java
+++ src/lektion4/Temperaturtabelle.java
@@ -1,12 +1,15 @@
 package lektion4;
 
 public class Temperaturtabelle {
-public static void main (String [] args);{
+    public static void main(String[] args) {
+        System.out.println("Fahrenheit\tCelsius");
 
 
+        for (int fahrenheit = 0; fahrenheit <= 300; fahrenheit++) {
+            double celsius = (5.0 / 9.0) * (fahrenheit - 32);
+
+
+            System.out.printf("%d\t\t%.2f%n", fahrenheit, celsius);
+        }
     }
-
-    public static double umrechnerF (double c){
-
-}
 }
