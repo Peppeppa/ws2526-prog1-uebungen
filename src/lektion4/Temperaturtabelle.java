863b256f86492088a4011149584428757e0c3c02
Stephen Eyring <accounts@eyring-s.de>
2025-11-03 18:24:58 +0100

    L4

--- src/lektion4/Temperaturtabelle.java
+++ src/lektion4/Temperaturtabelle.java
@@ -1,23 +1,23 @@
-package lektion4;
-
-public class Temperaturtabelle {
-
-    public static void main(String[] args) {
-        System.out.println("Fahrenheit\tCelsius");
-
-        for (int fahrenheit = 0; fahrenheit <= 300; fahrenheit++) {
-            double celsius = (5.0 / 9.0) * (fahrenheit - 32);
-
-public static void main (String[] args){
-    System.out.println("Fahrenheit " + " celsius");
-    for (int i = 0; i <= 300; i++){
-
-        System.out.println(i+'\t');
-
-            System.out.printf("%d\t\t%.2f%n", fahrenheit, celsius);
-        }
-    }
-
-}
-
-}
+//package lektion4;
+//
+//public class Temperaturtabelle {
+//
+//    public static void main(String[] args) {
+//        System.out.println("Fahrenheit\tCelsius");
+//
+//        for (int fahrenheit = 0; fahrenheit <= 300; fahrenheit++) {
+//            double celsius = (5.0 / 9.0) * (fahrenheit - 32);
+//
+//public static void main (String[] args){
+//    System.out.println("Fahrenheit " + " celsius");
+//    for (int i = 0; i <= 300; i++){
+//
+//        System.out.println(i+'\t');
+//
+//            System.out.printf("%d\t\t%.2f%n", fahrenheit, celsius);
+//        }
+//    }
+//
+//}
+//
+//}
