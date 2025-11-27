fe0f1cc96be66f7f97d89a7ddddba435f9093cec
Stephen Eyring <accounts@eyring-s.de>
2025-11-27 16:27:05 +0100

    L7

---
+++ src/lektion7/Umwandlung_inGroßbuchstaben.java
@@ -0,0 +1,27 @@
+package lektion7;
+import java.util.Scanner;
+
+public class Umwandlung_inGroßbuchstaben {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Bitte Text eingeben:");
+        String text = input.nextLine();
+
+        char[] arr = text.toCharArray();
+
+    }
+
+
+    public char[] inGrossbuchstaben(char[] input){
+
+        for (int i = 0 ; i < input.length ; i++){
+
+        }
+        return 0 ;
+
+
+    }
+
+}
