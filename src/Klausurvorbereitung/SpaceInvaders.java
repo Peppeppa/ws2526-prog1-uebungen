23341f798bf196917b289ec01acfdf62735f1a39
Stephen Eyring <accounts@eyring-s.de>
2026-01-07 13:39:02 +0100

    übung

---
+++ src/Klausurvorbereitung/SpaceInvaders.java
@@ -0,0 +1,27 @@
+package Klausurvorbereitung;
+
+public class SpaceInvaders {
+
+    private static final int ROWS = 5;
+    private static final int COLS = 8;
+
+    private final char[][] feld = new char[ROWS][COLS];
+    private int shipCol;
+
+    public SpaceInvaders() {
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c > COLS; c++){
+                feld[r][c] = ' ';
+            }
+        }
+
+        for (int c = 0; c < COLS; c++){
+            feld[0][c] = 'o';
+        }
+
+        shipCol = (int) (Math.random() * COLS);
+        feld[ROWS - 1][shipCol] = 'V';
+    }
+
+
+}
